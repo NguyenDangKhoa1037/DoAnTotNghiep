5cc66f5e8af4d3ca7af293f88f629dc62839b9fe
NguyenDangKhoa1037 <5851071037@st.utc2.edu.vn>
2022-04-08 23:37:30 +0700

    commit

--- Assets/Scripts/Enemies/Boong.cs
+++ Assets/Scripts/Enemies/Boong.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class Boong : Enemy
 {
     private Rigidbody2D myBody;
+    [SerializeField] GameObject bulletObject;
+    [SerializeField] float speedBullet;
+    private List<EnemyBullet> bullets = new List<EnemyBullet>();
     private void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
         if (player == null) findPlayer();
         this.speed += Random.Range(-0.1f, 0.1f);
+        for (int i = 0; i < 4; i++) {
+            int zAxis = Random.Range(0, 360);
+            EnemyBullet b = Instantiate(bulletObject, this.transform.position, Quaternion.Euler(0,0,zAxis)).GetComponent<EnemyBullet>();
+            b.gameObject.SetActive(false);
+            b.config(new BulletInfo(speedBullet, this.damage, 1));
+            bullets.Add(b);
+        }
     }
+
+
     private void Start()
     {
         float seconds = Random.Range(6f, 8f);
@@ -25,6 +37,11 @@ public class Boong : Enemy
 
 
     void explosion() {
+        for (int i = 0; i < 4; i++) {
+            bullets[i].transform.position = this.transform.position;
+            bullets[i].gameObject.SetActive(true);
+            bullets[i].startMove(Vector2.right);
+        }
         Destroy(gameObject);
     }
 }
